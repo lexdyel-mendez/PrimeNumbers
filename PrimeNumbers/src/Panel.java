d1602b1212e94caf36108c46f93a726c06967da7
lexdy <lexdy@Gamzee>
2017-02-09 22:12:42 -0400

    Begining

---
+++ PrimeNumbers/src/Panel.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2774962016745483881L;
+	
+	Panel(){
+		setBackground(Color.BLACK);
+	}
+}
---
+++ PrimeNumbers/src/Problem.java
@@ -0,0 +1,41 @@
+//Language: Java
+//Compiled Using: javac
+//Input for your program will be provided from STDIN
+//Print out all output from your program to STDOUT
+
+import java.util.Scanner;
+
+//Your submission should *ONLY* use the following class name
+public class Problem
+{
+    public static void main(String[] args)
+    {
+        Scanner stdin = new Scanner(System.in);
+       
+        int K;
+        int J;
+        int sum = 0;
+        
+        K = stdin.nextInt();
+        J = stdin.nextInt();
+        
+        while (K >= 1){
+        	if(K < J){
+        		if (Detecter.isPrime(K) == true){
+        			sum = sum + K;
+        			K++;
+        		}else{
+        			K++;
+        		}
+        			
+        	}else{
+        		break;
+        	}
+        }
+        
+        int result = sum + J;
+        System.out.print(result);
+        stdin.close();
+        
+    }	
+}
